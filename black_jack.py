3f018411b9c684a47d4b26489aa59d6064186ebb
RAMAN RANDIVE <145856583+RamanRed@users.noreply.github.com>
2024-09-28 16:27:15 +0530

    Update black_jack.py

--- black_jack.py
+++ black_jack.py
@@ -1,5 +1,6 @@
 from art import logo
 import random
+
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 user = []
 comp = []
@@ -23,15 +24,15 @@ def add_comp():
             break
 
 def show_u():
-    print("User cards are as follows:")
+    print(" user card as follows:")
     sum_u = 0
     for i in user:
         print(cards[i])
         if cards[i] == 11 and sum_u > 15:
             sum_u += 1
-        else:
+        else:    
             sum_u += cards[i]
-    print(f"Sum of user cards: {sum_u}")
+    print(f" sum of user cards : {sum_u}")
     return sum_u
 
 def sum_u():
@@ -39,25 +40,25 @@ def sum_u():
     for i in user:
         if cards[i] == 11 and sum_u > 15:
             sum_u += 1
-        else:
+        else:    
             sum_u += cards[i]
     return sum_u
 
 def sum_comp():
     sum_com = 0
-    for i in comp:
+    for i in comp: 
         sum_com += cards[i]
     if 1 in comp and sum_com >= 20:
         sum_com -= 10
-    print(f"Computer sum was: {sum_com}")
+    print(f" computer sum was : {sum_com}")
     return sum_com
 
 def show_user():
     for i in user:
-        print(f"User cards: {cards[i]}")
+        print(f" user cards:  {cards[i]}")
 
 def show_comp():
-    print(f"Computer's first card is: {cards[comp[0]]}")
+    print(f" computer first card is : {cards[comp[0]]}")
 
 print(logo)
 
@@ -67,42 +68,46 @@ for i in range(2):
 for i in range(2):
     add_comp()
 
-print("Cards distributed are:")
-show_comp()
-show_user()
+print("Cards distributed are : ")
 
-hit = input("Enter 'y' if user wants to hit else 'a': ").lower()
+show_comp()   
+show_user()   
+
+hit = input(" Enter 'y' if user want to hit else 'a' : ").lower()
 while hit == 'y' or hit == 'a':
     if hit == 'y':
         us = show_u()
         cs = sum_comp()
         if us == cs:
-            print(f"The game was a draw!!! \nWith score {us}")
+            print(f" the game was draw!!! \n with score {us} ") 
         elif us > cs and us <= 21:
-            print(f"Computer score was {cs}. Hence, the user wins the game with a score of {us}.")
+            print(f"  computer score was {cs} Hence, The user win the game by score of {us}  ")
         elif cs > us and cs > 21:
-            print(f"Computer score was {cs}. Hence, the user wins with a score of {us}.")
+            print(f"computer score was {cs} Hence, The user win with a score of {us}")
         elif us > cs and us > 21:
-            print(f"Computer score was {cs}. Hence, the user lost with a score of {us}.")
+            print(f"computer score was {cs} Hence, The user lost by score {us} ")  
         else:
-            print(f"Computer won the game with score {cs}. The user lost with score {us}.")
-        hit = input("If user wants more cards, enter 'y'. Else 'n'. Enter 'end' to exit the game:")
-         
+            print(f" computer won the game with score {cs}, The user lost by score {us}")   
+
+        hit = input(" if user want more cards Enter y else n \n Enter ' end '  to exit the game:")
+    
     elif hit == 'a':
         show_user()
         show_comp()
-        hit = input("If user wants more cards, enter 'a'. Else 'y' to HIT. Enter 'end' to exit the game:")
+        hit = input(" if user want more cards Enter 'a ' else 'y' to HIT  \n  Enter ' end '  to exit the game:")
         if hit == 'a':
             add_user()
-        sc = sum_comp()
+        sc = sum_comp()    
         if sc < 21:
             cn = random.randint(1, 10)
             if cn % 2 == 0: 
                 add_comp()
+        
         g = sum_u()
+        
         if g > 21:
-            print("User lost the game")
+            print(" user lost the game")
             show_user()
             break
 
-print("HOPE YOU ENJOYED THE GAME! *_*!")
+print(" HOPE  YOU ENJOYED THE GAME !*_*!")
